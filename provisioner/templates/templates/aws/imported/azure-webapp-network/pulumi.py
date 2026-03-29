10fe8e9172c4aded4bd6b7bd251c96cdec01927b
toolsaskarchie <tools@askarchie.io>
2026-03-28 22:06:14 -0500

    Add template: azure-webapp-network

--- provisioner/templates/templates/aws/imported/azure-webapp-network/pulumi.py
+++ provisioner/templates/templates/aws/imported/azure-webapp-network/pulumi.py
@@ -5,7 +5,7 @@ from provisioner.utils.aws import ResourceNamer, get_standard_tags
 from provisioner.templates.atomic_factory import PulumiAtomicFactory as factory
 
 @template_registry("azure-webapp-network")
-class AzureWebAppNetworkTemplate(InfrastructureTemplate):
+class AzureWebappNetwork(InfrastructureTemplate):
     def __init__(self, name=None, config=None, **kwargs):
         raw_config = config or kwargs or {}
         if name is None:
@@ -20,11 +20,10 @@ class AzureWebAppNetworkTemplate(InfrastructureTemplate):
         project = cfg('project_name', 'webapp')
         env = cfg('environment', 'dev')
         location = cfg('location', 'East US 2')
-
         tags = {
             'Environment': env,
             'Team': 'platform',
-            'ManagedBy': 'archie'
+            'ManagedBy': 'pulumi'
         }
 
         self.resource_group = factory.create('azure:resources:ResourceGroup', f'rg-{project}-{env}',
@@ -42,7 +41,7 @@ class AzureWebAppNetworkTemplate(InfrastructureTemplate):
         )
 
         self.subnet = factory.create('azure:network:Subnet', f'snet-{project}-{env}',
-            name=f'snet-{project}-{env}',
+            name='snet-app',
             resource_group_name=self.resource_group.name,
             virtual_network_name=self.virtual_network.name,
             address_prefixes=['10.0.1.0/24']
@@ -50,8 +49,8 @@ class AzureWebAppNetworkTemplate(InfrastructureTemplate):
 
         self.network_security_group = factory.create('azure:network:NetworkSecurityGroup', f'nsg-{project}-{env}',
             name=f'nsg-{project}-{env}',
-            location=location,
             resource_group_name=self.resource_group.name,
+            location=location,
             security_rules=[
                 {
                     'name': 'AllowHTTPS',
@@ -69,23 +68,26 @@ class AzureWebAppNetworkTemplate(InfrastructureTemplate):
         )
 
         pulumi.export('resource_group_name', self.resource_group.name)
-        pulumi.export('virtual_network_id', self.virtual_network.id)
-        pulumi.export('subnet_id', self.subnet.id)
+        pulumi.export('virtual_network_name', self.virtual_network.name)
+        pulumi.export('subnet_name', self.subnet.name)
+        pulumi.export('network_security_group_name', self.network_security_group.name)
+
         return self.get_outputs()
 
     def get_outputs(self):
         return {
             'resource_group_name': self.resource_group.name if hasattr(self, 'resource_group') else None,
-            'virtual_network_id': self.virtual_network.id if hasattr(self, 'virtual_network') else None,
-            'subnet_id': self.subnet.id if hasattr(self, 'subnet') else None
+            'virtual_network_name': self.virtual_network.name if hasattr(self, 'virtual_network') else None,
+            'subnet_name': self.subnet.name if hasattr(self, 'subnet') else None,
+            'network_security_group_name': self.network_security_group.name if hasattr(self, 'network_security_group') else None
         }
 
     @classmethod
     def get_metadata(cls):
         return {
             'name': 'azure-webapp-network',
-            'title': 'Azure WebApp Network',
-            'description': 'Configures network resources for Azure web application deployment',
+            'title': 'Azure WebApp Network Infrastructure',
+            'description': 'Creates an Azure resource group, virtual network, subnet, and network security group',
             'category': 'networking',
             'cloud': 'azure',
             'tier': 'standard'
