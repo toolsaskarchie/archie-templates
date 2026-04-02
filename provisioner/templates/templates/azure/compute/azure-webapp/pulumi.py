1059e34ff7592827cc528d9ca3c55e41089ad836
toolsaskarchie <tools@askarchie.io>
2026-04-02 09:07:08 -0500

    Add template: azure-webapp

--- provisioner/templates/templates/azure/compute/azure-webapp/pulumi.py
+++ provisioner/templates/templates/azure/compute/azure-webapp/pulumi.py
@@ -4,7 +4,7 @@ from provisioner.templates.base import InfrastructureTemplate, template_registry
 from provisioner.templates.atomic_factory import PulumiAtomicFactory as factory
 
 @template_registry("azure-webapp")
-class AzureWebapp(InfrastructureTemplate):
+class AzureWebAppTemplate(InfrastructureTemplate):
     def __init__(self, name=None, config=None, **kwargs):
         raw_config = config or kwargs or {}
         if name is None:
@@ -18,6 +18,7 @@ class AzureWebapp(InfrastructureTemplate):
 
         app_name = cfg('app_name', 'mywebapp')
         location = cfg('location', 'eastus')
+
         tags = {
             'Project': app_name,
             'Environment': 'dev',
@@ -33,33 +34,30 @@ class AzureWebapp(InfrastructureTemplate):
         self.service_plan = factory.create('azure-native:web:AppServicePlan', f'plan-{app_name}',
             resource_group_name=self.resource_group.name,
             location=location,
-            sku={
-                'name': 'B1'
-            },
-            kind='linux',
-            reserved=True,
+            sku_name='B1',
+            os_type='Linux',
             tags=tags
         )
 
-        self.webapp = factory.create('azure-native:web:WebApp', app_name,
+        self.web_app = factory.create('azure-native:web:WebApp', app_name,
             resource_group_name=self.resource_group.name,
             location=location,
             server_farm_id=self.service_plan.id,
-            site_config={
-                'linuxFxVersion': 'NODE:18-lts'
-            },
-            https_only=True,
+            site_config=azure_native.web.SiteConfigArgs(
+                linux_fx_version='NODE|18-lts',
+                https_only=True
+            ),
             tags=tags
         )
 
-        pulumi.export('webapp_url', f'https://{self.webapp.default_hostname}')
+        pulumi.export('webapp_url', self.web_app.default_hostname)
         pulumi.export('resource_group', self.resource_group.name)
 
         return self.get_outputs()
 
     def get_outputs(self):
         return {
-            'webapp_url': f'https://{self.webapp.default_hostname}' if hasattr(self, 'webapp') else None,
+            'webapp_url': self.web_app.default_hostname if hasattr(self, 'web_app') else None,
             'resource_group': self.resource_group.name if hasattr(self, 'resource_group') else None
         }
 
@@ -67,11 +65,23 @@ class AzureWebapp(InfrastructureTemplate):
     def get_metadata(cls):
         return {
             'name': 'azure-webapp',
-            'title': 'Web Application',
-            'description': 'Simple Azure Web App with Node.js',
+            'title': 'Web Application Deployment',
+            'description': 'Deploy a simple Linux Web App on Azure App Service',
             'category': 'compute',
             'cloud': 'azure',
             'tier': 'standard',
             'estimated_cost': '$10-20/mo',
-            'deployment_time': '~5 min'
+            'deployment_time': '~5 min',
+            'features': ['web hosting', 'linux container', 'scalable'],
+            'use_cases': ['web application', 'nodejs hosting'],
+            'pillars': ['performance', 'security'],
+            'resources': [
+                {"name": "Resource Group", "type": "azure-native:resources:ResourceGroup", "category": "management", "description": "Container for Azure resources"},
+                {"name": "App Service Plan", "type": "azure-native:web:AppServicePlan", "category": "compute", "description": "Defines compute resources for web app"},
+                {"name": "Web App", "type": "azure-native:web:WebApp", "category": "compute", "description": "Hosting environment for web application"}
+            ],
+            'config_fields': [
+                {"name": "app_name", "type": "text", "label": "Application Name", "required": true, "default": "mywebapp", "group": "General", "helpText": "Name of the web application"},
+                {"name": "location", "type": "select", "label": "Azure Region", "required": true, "default": "eastus", "group": "General", "helpText": "Azure region for deployment", "options": ["eastus", "westus", "eastus2", "westeurope"]}
+            ]
         }
